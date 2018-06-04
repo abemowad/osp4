feea81097e2bbd6f57b65dd73bf5e7d744dd0d89
Nathaniel Furbeyre <nmfurbearr@outlook.com>
2018-06-03 20:56:01 -0700

    - Renamed tinyfs.c to libTinyFS.c to agree with the project specs - Added TinyFS_errno.h with BLOCKSIZE defined to be 256 - Added stubs for the four functions required in libTinyFS.c - Began work on openDisk(filename, nBytes) for libTinyFS.c

---
+++ TinyFS_errno.h
@@ -0,0 +1,13 @@
+#ifndef TINYFS_ERRNO_H
+#define TINYFS_ERRNO_H
+
+/*
+ * A file containing a set of unified eror codes returned by the TinyFS
+ * interfaces. In addition, macros for the project are defined here.
+ */
+
+
+#define BLOCKSIZE 256
+
+
+#endif
